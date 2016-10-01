0eeae5b2344bdfd103b2f14ea8c24a761879d9b7
sylwia <sylwia.wacnik@gmail.com>
2016-10-01 11:16:29 +0200

    First

---
+++ app/src/main/java/pl/xcoding/sylwia/wpapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package pl.xcoding.sylwia.wpapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] listviewTitle = new String[]{
+            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4",
+            "ListView Title 5", "ListView Title 6"};
+
+    int[] listviewImage = new int[]{
+            R.drawable.image, R.drawable.image, R.drawable.image, R.drawable.image,R.drawable.image, R.drawable.image};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.list_view);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        ArrayList<AndroidVersion> av = prepareData();
+        AndroidDataAdapter mAdapter = new AndroidDataAdapter(getApplicationContext(), av);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+    }
+
+    private ArrayList<AndroidVersion> prepareData() {
+
+        ArrayList<AndroidVersion> av = new ArrayList<>();
+        for (int i = 0; i < listviewTitle.length; i++) {
+            AndroidVersion mAndroidVersion = new AndroidVersion();
+            mAndroidVersion.setAndroidVersionName(listviewTitle[i]);
+            mAndroidVersion.setrecyclerViewImage(listviewImage[i]);
+            av.add(mAndroidVersion);
+        }
+        return av;
+    }
+}
